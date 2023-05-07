a7f72dd016cea2d006a3034a2b07d90b3cbb9492
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-05-07 17:45:14 +0200

    doInBackground

--- app/src/main/java/com/example/flickrapp/GetImageOnClickListener.java
+++ app/src/main/java/com/example/flickrapp/GetImageOnClickListener.java
@@ -3,6 +3,16 @@ package com.example.flickrapp;
 import android.os.AsyncTask;
 import android.view.View;
 
+import androidx.arch.core.internal.SafeIterableMap;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class GetImageOnClickListener implements View.OnClickListener {
 
@@ -13,6 +23,34 @@ public class GetImageOnClickListener implements View.OnClickListener {
 
     private class AsyncFlickrJSONData extends AsyncTask<String, Void, String> {
 
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                String s = readStream(in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
 
+        @Override
+        protected void onPostExecute(String result) {
+            // parse JSON response and handle the image download
+            // ...
+        }
+    }
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        return sb.toString();
     }
 }
