575dec7c3c956a5cf44e6332d2af9a0af59b610f
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-05-09 10:28:22 +0200

    Bitmap doInBackground

--- app/src/main/java/com/example/flickrapp/AsyncBitmapDownloader.java
+++ app/src/main/java/com/example/flickrapp/AsyncBitmapDownloader.java
@@ -1,8 +1,46 @@
 package com.example.flickrapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AsyncBitmapDownloader extends AsyncTask<String, Void, Bitmap> {
-    
+    private ImageView image;
+
+    public AsyncBitmapDownloader(ImageView image) {
+        this.image = image;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        int n = strings.length;
+        Bitmap bitmap = null;
+        for (int i = 0; i < n; i++) {
+            try {
+                URL url = new URL(strings[i]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream s = new BufferedInputStream(urlConnection.getInputStream());
+                    bitmap = BitmapFactory.decodeStream(s);
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (isCancelled()) break;
+        }
+        return bitmap;
+    }
 }
+
