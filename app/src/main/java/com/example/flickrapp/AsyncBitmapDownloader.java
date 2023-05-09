38c2d6fc7960eaf8434f2e16a63d21351f61ab1e
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-05-09 10:19:42 +0200

    Downloader

---
+++ app/src/main/java/com/example/flickrapp/AsyncBitmapDownloader.java
@@ -0,0 +1,8 @@
+package com.example.flickrapp;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+public class AsyncBitmapDownloader extends AsyncTask<String, Void, Bitmap> {
+    
+}
