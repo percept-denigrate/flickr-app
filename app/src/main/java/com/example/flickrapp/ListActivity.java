d5f6c946a4f67f0226fe75bd8ee0925e811ff3ff
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-05-10 10:54:08 +0200

    Correction id

--- app/src/main/java/com/example/flickrapp/ListActivity.java
+++ app/src/main/java/com/example/flickrapp/ListActivity.java
@@ -3,6 +3,8 @@ package com.example.flickrapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -19,5 +21,24 @@ public class ListActivity extends AppCompatActivity {
 
     private class Adapter extends BaseAdapter{
 
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
