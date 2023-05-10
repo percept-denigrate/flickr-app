130140dfbddd26dd10ea88d69571a4c547e79624
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-05-10 10:50:55 +0200

    Adapter

--- app/src/main/java/com/example/flickrapp/ListActivity.java
+++ app/src/main/java/com/example/flickrapp/ListActivity.java
@@ -3,6 +3,9 @@ package com.example.flickrapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ListView listView = (ListView) findViewById(R.id.list);
+        Adapter adapter = new Adapter();
+    }
+
+    private class Adapter extends BaseAdapter{
+
     }
 }
