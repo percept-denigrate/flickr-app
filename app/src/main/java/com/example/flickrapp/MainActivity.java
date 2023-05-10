6cb9c541b5eee3dbcd821e5ff5f0934be57a2ee1
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-05-10 10:37:43 +0200

    Correction nom de l'activité

--- app/src/main/java/com/example/flickrapp/MainActivity.java
+++ app/src/main/java/com/example/flickrapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.flickrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button changeActivity = (Button) findViewById(R.id.changeActivity);
+        changeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callListActivity = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(callListActivity);
+            }
+        });
 
         Button getImageButton = findViewById(R.id.button);
         ImageView image = (ImageView) findViewById(R.id.image);
